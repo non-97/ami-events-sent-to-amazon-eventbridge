e98aaf5cc11e0397d8c15381b2ff463d1048493b
non-97 <nonmonaka07@gmail.com>
2022-05-10 09:59:34 +0900

    Initial commit

---
+++ test/ami-events.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmiEvents from '../lib/ami-events-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ami-events-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmiEvents.AmiEventsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
